efbf53f2edef2e6ac9f7937b88352add16c3da5c
Prathikson <prathik@vincellstudios.com>
2025-05-03 12:24:04 -0600

    Hero Secton complted with stats counter and animations Gsap timeline: 1:21:22

--- src/constants/index.js
+++ src/constants/index.js
@@ -29,9 +29,9 @@ const words = [
 ];
 
 const counterItems = [
-  { value: 15, suffix: "+", label: "Years of Experience" },
-  { value: 200, suffix: "+", label: "Satisfied Clients" },
-  { value: 108, suffix: "+", label: "Completed Projects" },
+  { value: 2, suffix: "+", label: "Years of Experience" },
+  { value: 10, suffix: "+", label: "Satisfied Clients" },
+  { value: 10, suffix: "+", label: "Completed Projects" },
   { value: 90, suffix: "%", label: "Client Retention Rate" },
 ];
 
@@ -262,6 +262,8 @@ const socialImgs = [
     imgPath: "/images/linkedin.png",
   },
 ];
+
+
 
 export {
   words,
