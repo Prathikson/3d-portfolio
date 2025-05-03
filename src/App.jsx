de6bbce4b4e35ad97e7aece9a45c17ce64aaae4a
Prathikson <prathik@vincellstudios.com>
2025-05-03 14:10:24 -0600

    Nav abr completed timestamp 1:42:30

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from './sections/Hero'
+import ShowcaseSection from './sections/ShowcaseSection'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <>
+    <Navbar/>
     <Hero />
+    <ShowcaseSection/>
     </>
   )
 }
