4c5901ae52f47a1d6808121987e6a93b5b17620f
Prathikson <prathik@vincellstudios.com>
2025-05-05 22:16:47 -0600

    Chabe in contact section

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -107,8 +107,8 @@ const Contact = () => {
             </div>
           </div>
           <div className="xl:col-span-7 min-h-96">
-            <div className="bg-[#cd7c2e] w-full h-full hover:cursor-grab rounded-3xl overflow-hidden">
-              <ContactExperience/>
+            <div className="bg-[#1b1b1b] w-full h-full hover:cursor-grab rounded-3xl overflow-hidden justify-center">
+              <img src="/images/LogoContact.png" alt="logo"/>
             </div>
           </div>
         </div>
